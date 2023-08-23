03bc6b1eafdc1dfdad43b3ac98d4ad6a8b9b29a7
Caio Edu <84157848+caioed77@users.noreply.github.com>
2023-08-23 16:43:45 -0300

    adicionando a class security config

---
+++ api-multbanc/src/main/java/com/multBancapp/apimultbanc/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.multBancapp.apimultbanc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .securityMatcher("/auth/**")
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
--- api-multbanc/src/main/java/com/multBancapp/apimultbanc/services/UserService.java
+++ api-multbanc/src/main/java/com/multBancapp/apimultbanc/services/UserService.java
@@ -1,11 +1,13 @@
 package com.multBancapp.apimultbanc.services;
 
+import com.multBancapp.apimultbanc.config.SecurityConfig;
 import com.multBancapp.apimultbanc.entities.UserEntity;
 import com.multBancapp.apimultbanc.exceptions.BusinessRulesException;
-import com.multBancapp.apimultbanc.models.UserDTO;
+import com.multBancapp.apimultbanc.models.dto.UserDTO;
 import com.multBancapp.apimultbanc.repositories.UserRepository;
 import com.multBancapp.apimultbanc.services.utils.ValidateCPF;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,6 +17,9 @@ import java.util.Optional;
 public class UserService {
 
       private final UserRepository userRepository;
+
+      @Autowired
+      private SecurityConfig passwordEncoder;
 
       @Autowired
       public UserService(UserRepository userRepository){
@@ -36,6 +41,9 @@ public class UserService {
             var validadeCPF = new ValidateCPF();
 
             if (validadeCPF.validade(user)) {
+                  var encryptedPassword = passwordEncoder.passwordEncoder().encode(user.getPassword());
+                  user.setPassword(encryptedPassword);
+
                   return userRepository.save(user);
             } else {
                   throw new BusinessRulesException("CPF Invalido");
