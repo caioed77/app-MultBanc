c06f0910adc1468489ec661a7f9ee711b2cc47bb
Caio Eduardo <84157848+caioed77@users.noreply.github.com>
2023-08-26 00:23:57 -0300

    desenvolvendo tela de login

--- api-multbanc/src/main/java/com/multBancapp/apimultbanc/exceptions/BusinessRulesException.java
+++ api-multbanc/src/main/java/com/multBancapp/apimultbanc/exceptions/BusinessRulesException.java
@@ -1,7 +1,6 @@
 package com.multBancapp.apimultbanc.exceptions;
 
 public class BusinessRulesException extends RuntimeException{
-
             public BusinessRulesException(String msg){
                   super(msg);
             }
--- web-multbanc/src/components/NavBar.tsx
+++ web-multbanc/src/components/NavBar.tsx
@@ -1,7 +1,7 @@
 export default function NavBar() {
     return (
         <div className="z-10 max-w-auto w-full font-mono text-sm lg:flex">
-            <p className="fixed left-0 top-0 flex w-full justify-start items-center bg-black h-32">
+            <p className="fixed left-0 top-0 flex w-full justify-start items-center bg-[#2C0D46] h-32">
                 <span className="m-10 text-white font-bold text-4xl"> MultBanc&nbsp;</span>
             </p>
         </div>
